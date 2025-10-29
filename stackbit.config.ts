58df8a6dc21ecdbb2d229291b7c083c0ad253485
partypalhk <partypalhk@gmail.com>
2025-10-30 00:15:50 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,137 +1,49 @@
-Oct 29, 11:48:29 PM: build-image version: 235a96ed6c11cbdf942eed7e70e6802956f9b464 (focal)
-Oct 29, 11:48:29 PM: devbot version: f7382f2a45aaa2b55f6e36f1c92bb8dd2210123b
-Oct 29, 11:48:29 PM: Fetching cached dependencies
-Oct 29, 11:48:29 PM: Starting to download cache of 109.9MB
-Oct 29, 11:48:30 PM: Finished downloading cache in 1.2s
-Oct 29, 11:48:30 PM: Starting to extract cache
-Oct 29, 11:48:31 PM: Finished extracting cache in 1.057s
-Oct 29, 11:48:31 PM: Finished fetching cache in 2.303s
-Oct 29, 11:48:31 PM: Starting to prepare your repo
-Oct 29, 11:48:31 PM: Preparing Git Reference main
-Oct 29, 11:48:32 PM: Fetching 0 asset(s)
-Oct 29, 11:48:32 PM: Starting to install dependencies
-Oct 29, 11:48:33 PM: [2mmise[0m [34mpython[0m@3.8.20   install
-Oct 29, 11:48:33 PM: [2mmise[0m [34mpython[0m@3.8.20   download cpython-3.8.20+20241002-x86_64-unknown-linux-gnu-install_only_stripped.tar.gz
-Oct 29, 11:48:33 PM: [2mmise[0m [34mpython[0m@3.8.20   extract cpython-3.8.20+20241002-x86_64-unknown-linux-gnu-install_only_stripped.tar.gz
-Oct 29, 11:48:34 PM: [2mmise[0m [34mpython[0m@3.8.20   python --version
-Oct 29, 11:48:34 PM: [2mmise[0m [34mpython[0m@3.8.20   Python 3.8.20
-Oct 29, 11:48:34 PM: [2mmise[0m [34mpython[0m@3.8.20 [38;5;10m✓[0m installed
-Oct 29, 11:48:34 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:34 PM: Python version set to 3.8
-Oct 29, 11:48:34 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:34 PM: Attempting Ruby version 2.7.2, read from environment
-Oct 29, 11:48:34 PM: Using Ruby version 2.7.2
-Oct 29, 11:48:35 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:35 PM: Started restoring cached go cache
-Oct 29, 11:48:35 PM: Finished restoring cached go cache
-Oct 29, 11:48:35 PM: Installing Go version 1.19.5 (requested 1.19.5)
-Oct 29, 11:48:40 PM: go version go1.19.5 linux/amd64
-Oct 29, 11:48:40 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:40 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:40 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:40 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:40 PM: Using PHP version 8.0
-Oct 29, 11:48:40 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:41 PM: Started restoring cached Node.js version
-Oct 29, 11:48:42 PM: Finished restoring cached Node.js version
-Oct 29, 11:48:43 PM: v22.21.1 is already installed.
-Oct 29, 11:48:43 PM: Now using node v22.21.1 (npm v10.9.4)
-Oct 29, 11:48:43 PM: Enabling Node.js Corepack
-Oct 29, 11:48:43 PM: Started restoring cached build plugins
-Oct 29, 11:48:43 PM: Finished restoring cached build plugins
-Oct 29, 11:48:44 PM: Successfully installed dependencies
-Oct 29, 11:48:44 PM: Starting dev server
-Oct 29, 11:48:44 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:44 PM: /opt/build/env_store/.env: line 10: mise: command not found
-Oct 29, 11:48:44 PM: mise [36m/opt/build/repo/mise.toml[0m tools: [34mpython[0m@3.8.20
-Oct 29, 11:48:48 PM: Uploading Cache of size 110.1MB
-Oct 29, 11:48:49 PM: ⬥ Ignored project settings env var: STACKBIT_API_SECRET (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: SITE_ID (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: SITE_NAME (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: DEPLOY_ID (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: BUILD_ID (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: URL (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: REPOSITORY_URL (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: CONTEXT (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: BRANCH (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: HEAD (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: COMMIT_REF (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: CACHED_COMMIT_REF (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: PULL_REQUEST (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: LANG (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: LANGUAGE (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: LC_ALL (defined in process)
-Oct 29, 11:48:49 PM: ⬥ Ignored general context env var: NEXT_TELEMETRY_DISABLED (defined in process)
-Oct 29, 11:48:49 PM: ⬥ No app server detected. Using simple static server
-Oct 29, 11:48:49 PM: ⬥ Running static server from "repo"
-Oct 29, 11:48:49 PM: ⬥ Setting up local dev server
-Oct 29, 11:48:53 PM: origin
-Oct 29, 11:48:53 PM: [dev-server-plugin] listening on port 8288
-Oct 29, 11:48:54 PM: [32minfo[39m: Site directory: /opt/build/repo
-Oct 29, 11:48:54 PM: [32minfo[39m: Loading configuration from https://api-create.services.netlify.com
-Oct 29, 11:48:54 PM: [32minfo[39m: localId: 06b176e8e3704ababdd8a86134b4c40f
-Oct 29, 11:48:54 PM: [32minfo[39m: Content sources found: git (project: 86024cad, version: 1.0.35)
-Oct 29, 11:48:54 PM: [33mwarn[39m: error in stackbit config: "mapModels" must be of type function
-Oct 29, 11:48:54 PM: [32minfo[39m: Using Content Source Interface
-Oct 29, 11:48:54 PM: [32m[32minfo[32m[39m: [content-store] Initializing content source: git (project: 86024cad)
-Oct 29, 11:48:54 PM: [33m[33mwarn[33m[39m: [cms-fs] No model found with name: 'digital'
-Oct 29, 11:48:54 PM: [33m[33mwarn[33m[39m: [cms-fs] Error converting file to document: dino-planner.json
-Oct 29, 11:48:54 PM: [31merror[39m: Error starting Stackbit dev server:
-Oct 29, 11:48:54 PM: Error: ENOENT: no such file or directory, scandir '/opt/build/repo/public'
-Oct 29, 11:48:56 PM: [91m[1m​[22m[39m
-Oct 29, 11:48:56 PM: [91m[1mPlugin "@netlify/plugin-visual-editor" internal error         [22m[39m
-Oct 29, 11:48:56 PM: [91m[1m────────────────────────────────────────────────────────────────[22m[39m
-Oct 29, 11:48:56 PM: ​
-Oct 29, 11:48:56 PM:   [31m[1mError message[22m[39m
-Oct 29, 11:48:56 PM:   Error: Error starting Stackbit dev server
-Oct 29, 11:48:56 PM: ​
-Oct 29, 11:48:56 PM:   [31m[1mPlugin details[22m[39m
-Oct 29, 11:48:56 PM:   Package:        @netlify/plugin-visual-editor
-Oct 29, 11:48:56 PM:   Version:        1.4.1
-Oct 29, 11:48:56 PM:   Repository:     git+https://github.com/netlify/netlify-plugin-visual-editor.git
-Oct 29, 11:48:56 PM:   npm link:       https://www.npmjs.com/package/@netlify/plugin-visual-editor
-Oct 29, 11:48:56 PM:   Report issues:  https://github.com/netlify/netlify-plugin-visual-editor/issues
-Oct 29, 11:48:56 PM: ​
-Oct 29, 11:48:56 PM:   [31m[1mError location[22m[39m
-Oct 29, 11:48:56 PM:   In "onPreDev" event in "@netlify/plugin-visual-editor" from netlify.toml and package.json
-Oct 29, 11:48:56 PM:       at start (/opt/buildhome/node-deps/node_modules/@stackbit/dev/dist/dev.js:121:15)
-Oct 29, 11:48:56 PM:       at async onPreDev (file:///opt/buildhome/node-deps/node_modules/@netlify/plugin-visual-editor/dist/main.js:20:28)
-Oct 29, 11:48:56 PM:       at async file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/child/run.js:32:9
-Oct 29, 11:48:56 PM:       at async handleEvent (file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/child/main.js:28:42)
-Oct 29, 11:48:56 PM:       at async process.<anonymous> (file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/ipc.js:91:24)
-Oct 29, 11:48:56 PM: ​
-Oct 29, 11:48:56 PM:   [31m[1mResolved config[22m[39m
-Oct 29, 11:48:56 PM:   build:
-Oct 29, 11:48:56 PM:     publish: /opt/build/repo
-Oct 29, 11:48:56 PM:     publishOrigin: config
-Oct 29, 11:48:56 PM:   redirects:
-Oct 29, 11:48:56 PM:     - from: /.ntlfy-dev/*
-      headers:
-        x-nf-hidden-proxy: "true"
-      status: 200
-      to: http://localhost:8288/:splat
-  redirectsOrigin: inline
- ›   Error: Could not start local development server
-Oct 29, 11:48:56 PM: Error starting Stackbit dev server
-Oct 29, 11:48:56 PM: Error: Error starting Stackbit dev server
-Oct 29, 11:48:56 PM:     at start (/opt/buildhome/node-deps/node_modules/@stackbit/dev/dist/dev.js:121:15)
-Oct 29, 11:48:56 PM:     at async onPreDev (file:///opt/buildhome/node-deps/node_modules/@netlify/plugin-visual-editor/dist/main.js:20:28)
-Oct 29, 11:48:56 PM:     at async file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/child/run.js:32:9
-Oct 29, 11:48:56 PM:     at async handleEvent (file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/child/main.js:28:42)
-Oct 29, 11:48:56 PM:     at async process.<anonymous> (file:///opt/buildhome/node-deps/node_modules/netlify-cli/node_modules/@netlify/build/lib/plugins/ipc.js:91:24)
-Oct 29, 11:48:56 PM: Error running command: exit status 1
-Oct 29, 11:48:56 PM: Finished processing dev server in 27.611s
-Oct 29, 11:48:56 PM: ==================================================================
-Oct 29, 11:48:56 PM:
-Oct 29, 11:48:58 PM: build-image version: 235a96ed6c11cbdf942eed7e70e6802956f9b464 (focal)
-Oct 29, 11:48:58 PM: devbot version: f7382f2a45aaa2b55f6e36f1c92bb8dd2210123b
-Oct 29, 11:48:58 PM: Fetching cached dependencies
-Oct 29, 11:48:58 PM: ==================================================================
-Oct 29, 11:48:58 PM: Finished processing dev server in 132ms
-Oct 29, 11:48:58 PM:
-Oct 29, 11:48:58 PM: Failed to fetch cache, continuing with dev run
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.7.0",
+  ssgName: "custom",
+  nodeVersion: "18",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["products"],
+      models: [
+        {
+          name: "PartyKit",
+          type: "data",
+          label: "Party Kit",
+          filePath: "products/{slug}.json",
+          fields: [
+            { name: "name", type: "string", label: "Name", required: true },
+            { name: "price", type: "number", label: "Price (HK$)", required: true },
+            { name: "img", type: "image", label: "Image" },
+            { name: "desc", type: "text", label: "Description" }
+          ]
+        },
+        {
+          name: "Page",
+          type: "page",
+          label: "Homepage",
+          urlPath: "/",
+          filePath: "index.html",
+          fields: [
+            { name: "title", type: "string", label: "Title" },
+            { name: "subtitle", type: "string", label: "Subtitle" },
+            { name: "tagline", type: "string", label: "Tagline" },
+            { name: "checkoutTitle", type: "string", label: "Checkout Title" },
+            { name: "checkoutQR", type: "image", label: "PayMe QR" },
+            { name: "checkoutInstruction", type: "string", label: "Instruction" }
+          ]
+        }
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ]
+});
